8bcd893708e6daabdea9d130945441790a5cdda2
cb-mohamedsullaiman <39910807+cb-mohamedsullaiman@users.noreply.github.com>
2018-06-06 14:34:39 +0530

    Delete Add2num.java

--- week/week1/day1/Add2num.java
+++
@@ -1,9 +0,0 @@
-public class Add2num
-{
-	public static void main(String args[])
-	{
-		int x=5,y=8;
-		z=x+y;                  //Adding x and y
-		System.out.printn(z);  //Printing the number within the bracket
- 	}
-}
